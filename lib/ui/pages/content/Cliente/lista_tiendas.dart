35726e379bfc0d3b817454d5ac804b24d61adeda
fGraphite <110121728+fGraphite@users.noreply.github.com>
2022-10-20 11:14:11 -0500

    añadir archivo

---
+++ lib/ui/pages/content/Cliente/lista_tiendas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListTiendas extends StatefulWidget {
+  const ListTiendas({super.key});
+
+  @override
+  State<ListTiendas> createState() => _ListTiendasState();
+}
+
+class _ListTiendasState extends State<ListTiendas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
