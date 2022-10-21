9ae506128382f0808d146eb9352cf8e6698f3f2c
fGraphite <110121728+fGraphite@users.noreply.github.com>
2022-10-20 20:31:51 -0500

    carrito incompleto

--- lib/ui/pages/content/Cliente/shopping_cart.dart
+++ lib/ui/pages/content/Cliente/shopping_cart.dart
@@ -21,6 +21,76 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint("presionado");
+              },
+              child: SizedBox(
+                width: 300,
+                height: 90,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Icon(Icons.apple),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Text("Frutas"),
+                            Text("Manzana (1Kg)"),
+                            Text("Precio: 1837/kg"),
+                          ],
+                        ),
+                        const Text("7")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint("presionado");
+              },
+              child: SizedBox(
+                width: 300,
+                height: 90,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.local_police),
+                    Column(
+                      children: [
+                        const Icon(Icons.apple),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Text("Carnes"),
+                            Text("Pollo (1Kg)"),
+                            Text("Precio: 1950/kg"),
+                          ],
+                        ),
+                        const Text("7")
+                      ],
+                    ),
+                    const Icon(Icons.favorite)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
