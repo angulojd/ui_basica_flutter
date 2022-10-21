a86b6f8a051843e6dd23d26f2b00110209438fb0
fGraphite <110121728+fGraphite@users.noreply.github.com>
2022-10-21 06:57:07 -0500

    CategoryVIew

--- lib/ui/pages/content/Cliente/lista_tiendas.dart
+++ lib/ui/pages/content/Cliente/lista_tiendas.dart
@@ -1,5 +1,6 @@
 import 'package:f_testing_template/ui/pages/content/Cliente/segunda_pantalla.dart';
 import 'package:f_testing_template/ui/pages/content/Cliente/shopping_cart.dart';
+import 'package:f_testing_template/ui/pages/content/Cliente/vista_categorias.dart';
 import 'package:flutter/material.dart';
 
 class ListTiendas extends StatefulWidget {
@@ -13,144 +14,184 @@ class _ListTiendasState extends State<ListTiendas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Vista de Cliente"),
-          automaticallyImplyLeading: false,
-          leading: IconButton(
+      appBar: AppBar(
+        title: const Text("TIENDAS"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          TextButton(
             onPressed: () {
-              Navigator.of(context).pop();
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const ProductListPage();
+                  },
+                ),
+              );
             },
-            icon: const Icon(Icons.arrow_back_ios),
+            child: const Text(
+              "Cra 70 #41-9",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const ProductListPage();
-                    },
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const ShoppingCartPage();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.shopping_basket_outlined),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Que Desea buscar?",
+                        hintText: "Escribe un producto que quieras comprar"),
                   ),
-                );
-              },
-              child: const Text(
-                "Cra 70 #41-9",
-                style: TextStyle(color: Colors.white),
+                ),
+                Icon(Icons.search),
+                Icon(Icons.settings_outlined)
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text("Tiendas Disponibles:"),
+                SizedBox(width: 100),
+                SizedBox(width: 100),
+              ],
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const CategoryViewPage();
+                      },
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(
+                      Icons.local_police,
+                      size: 50,
+                    ),
+                    Column(
+                      children: const [
+                        Text("Tienda los toros"),
+                        Text("Cra 43 # 45-98"),
+                        Text("7.2* / 1.5km ")
+                      ],
+                    ),
+                    const Icon(
+                      Icons.favorite_border,
+                      size: 50,
+                    )
+                  ],
+                ),
               ),
             ),
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const ShoppingCartPage();
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(Icons.shopping_basket_outlined),
-            )
+            const SizedBox(height: 30),
+            SizedBox(
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const CategoryViewPage();
+                      },
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(
+                      Icons.local_pharmacy,
+                      size: 50,
+                    ),
+                    Column(
+                      children: const [
+                        Text("Tienda los pericos"),
+                        Text("Cra 34 # 84-47"),
+                        Text("9.0* / 3.4km ")
+                      ],
+                    ),
+                    const Icon(
+                      Icons.favorite_border,
+                      size: 50,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const CategoryViewPage();
+                      },
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(
+                      Icons.shield,
+                      size: 50,
+                    ),
+                    Column(
+                      children: const [
+                        Text("Tienda La Nacional"),
+                        Text("Cra 12 # 78-105"),
+                        Text("10.0* / 5km ")
+                      ],
+                    ),
+                    const Icon(
+                      Icons.favorite_border,
+                      size: 50,
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  SizedBox(
-                    height: 50,
-                    width: 300,
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Que Desea buscar?",
-                          hintText: "Escribe un producto que quieras comprar"),
-                    ),
-                  ),
-                  Icon(Icons.search),
-                  Icon(Icons.settings_outlined)
-                ],
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text("Tiendas Disponibles:"),
-                  SizedBox(width: 100),
-                  SizedBox(width: 100),
-                ],
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 400,
-                child: ElevatedButton(
-                    onPressed: () {
-                      debugPrint("you have pressed store 1");
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Icon(Icons.local_police),
-                        Column(
-                          children: const [
-                            Text("Tienda los toros"),
-                            Text("Cra 43 # 45-98"),
-                            Text("7.2* / 1.5km ")
-                          ],
-                        ),
-                        const Icon(Icons.favorite)
-                      ],
-                    )),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 400,
-                child: ElevatedButton(
-                    onPressed: () {
-                      debugPrint("you have pressed store 1");
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Icon(Icons.local_pharmacy),
-                        Column(
-                          children: const [
-                            Text("Tienda los pericos"),
-                            Text("Cra 34 # 84-47"),
-                            Text("9.0* / 3.4km ")
-                          ],
-                        ),
-                        const Icon(Icons.favorite)
-                      ],
-                    )),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 400,
-                child: ElevatedButton(
-                    onPressed: () {
-                      debugPrint("you have pressed store 1");
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Icon(Icons.shield),
-                        Column(
-                          children: const [
-                            Text("Tienda La Nacional"),
-                            Text("Cra 12 # 78-105"),
-                            Text("10.0* / 5km ")
-                          ],
-                        ),
-                        const Icon(Icons.favorite)
-                      ],
-                    )),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
---
+++ lib/ui/pages/content/Cliente/vista_categorias.dart
@@ -0,0 +1,181 @@
+import 'package:f_testing_template/ui/pages/content/Cliente/segunda_pantalla.dart';
+import 'package:f_testing_template/ui/pages/content/Cliente/shopping_cart.dart';
+import 'package:flutter/material.dart';
+
+class CategoryViewPage extends StatefulWidget {
+  const CategoryViewPage({super.key});
+
+  @override
+  State<CategoryViewPage> createState() => _CategoryViewPageState();
+}
+
+class _CategoryViewPageState extends State<CategoryViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CATEGORIAS"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const ProductListPage();
+                  },
+                ),
+              );
+            },
+            child: const Text(
+              "Cra 70 #41-9",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const ShoppingCartPage();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.shopping_basket_outlined),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Que Desea buscar?",
+                        hintText: "Escribe un producto que quieras comprar"),
+                  ),
+                ),
+                Icon(Icons.search),
+                Icon(Icons.settings_outlined)
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text("Categorias Disponibles:"),
+                SizedBox(width: 100),
+                SizedBox(width: 100),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 80,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        width: 80,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              debugPrint("debugdebug");
+                            },
+                            child: const Icon(
+                              Icons.apple,
+                              size: 50,
+                            )),
+                      ),
+                      const Text("Frutas")
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 80,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        width: 80,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              debugPrint("debugdebug");
+                            },
+                            child: const Icon(
+                              Icons.wind_power_outlined,
+                              size: 50,
+                            )),
+                      ),
+                      const Text("Abanicos")
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 80,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        width: 80,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              debugPrint("debugdebug");
+                            },
+                            child: const Icon(
+                              Icons.wine_bar,
+                              size: 50,
+                            )),
+                      ),
+                      const Text("Vinos")
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 80,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        width: 80,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              debugPrint("debugdebug");
+                            },
+                            child: const Icon(
+                              Icons.attach_file_sharp,
+                              size: 50,
+                            )),
+                      ),
+                      const Text("Clips")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
